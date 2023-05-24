27368388d308d473903c310bb72dfb871b87648c
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2023-05-24 16:49:18 -0400

    restSpreadOperatorFunctions

---
+++ app.js
@@ -0,0 +1,52 @@
+// function filterOutOdds() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.filter(function(num) {
+//       return num % 2 === 0
+//     });
+//   }
+
+  //rewritten
+
+const filterOutOdds = (...args) => args.filter(v => v % 2 === 0);
+
+const findMin = (...args) => Math.min(...args)
+
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
+
+const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(val => val*2)]
+
+
+
+const removeRandom = items => {
+    let idx = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, idx), ...items.slice(idx + 1)];
+}
+
+const extend = (arr1, arr2) => ({...arr1, ...arr2})
+
+const addKeyVal = (obj, key, val) => {
+
+    // OPTION 1
+    let newObj = { ...obj }
+    newObj[key] = val;
+    return newObj;
+}
+
+const removeKey = (obj, key) => {
+
+    // OPTION 1
+    let newObj = { ...obj }
+    delete newObj[key]
+    return newObj;
+}
+
+
+const combine = (obj1, obj2) => {
+    return { ...obj1, ...obj2 };
+}
+
+const update = (obj, key, val) => {
+    let newObj = { ...obj }
+    newObj[key] = val;
+    return newObj;
+}
